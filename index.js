737f53aabfa5b71eb646669971a997786d74985f
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-04-08 02:04:38 -0400

    added psql, postgres

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const express = require('express')
+const {Pool} = require('pg')
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
 
-let notes = []
-
-app.get('/', (req, res) => {
-    res.status(201).send(notes)
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'notesdb',
+    password: 'JUostgres@08',
+    port: 5432
 })
 
-app.post('/notes', (req, res) => {
+app.get('/notes', async (req, res) => {
+    const result = await pool.query('SELECT * FROM notes')
+    res.status(201).send(result.rows)
+})
+
+app.post('/notes', async (req, res) => {
     const {title, content} = req.body
-    const newNote = {id: notes.length + 1, title, content}
-    notes.push(newNote)
-    res.status(201).send(newNote)
+    const result = await pool.query('INSERT INTO notes(title, content) VALUES($1, $2) RETURNING *', [title, content])
+    res.status(201).send(result.rows[0])
 })
 
-app.put('/notes/:id', (req, res) => {
+app.put('/notes/:id', async (req, res) => {
     const {id} = req.params
     const {title, content} = req.body
-    let note = notes.find(note => note.id === parseInt(id))
-    if (note) {
-        note.title = title
-        note.content = content
-        res.status(200).send(note)
-    } else {
-        res.status(404).send({message: "Note not found"})
-    }
+    const result = await pool.query('UPDATE notes SET title = $1, content = $2 WHERE id = $3 RETURNING *', [title, content, id])
+    res.status(200).send(result.rows[0])
 })
 
-app.delete('/notes/:id', (req, res) => {
+app.delete('/notes/:id', async (req, res) => {
     const {id} = req.params 
-    notes = notes.filter(note => note.id !== parseInt(id))
+    await pool.query('DELETE FROM notes WHERE id = $1', [id])
     res.status(204).send()
 })
 
